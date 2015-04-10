242942d8b703c5e761d6913ee57b234ea9190fa0
Velsu <riftmage50@gmail.com>
2015-04-10 16:04:03 +0200

    Add Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,21 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-  end
+    assert_select "title", "Home | Microblog Sample App"
+	end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Microblog Sample App"
+
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About | Microblog Sample App"
+
   end
 
 end
