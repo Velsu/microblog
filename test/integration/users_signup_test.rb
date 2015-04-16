82df529c9a41fb0ab2a809d0c2766e3071da75bb
Velsu <riftmage50@gmail.com>
2015-04-16 13:36:47 +0200

    Enable SSL and Puma webserver for Production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,17 +2,35 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-	test "User.count" do
+	test "Should be unable to sign up" do
 		get signup_path
-		before_count = User.count 
-		post users_path, user: { name: "   ",
+		assert_no_difference "User.count" do
+			post users_path, user: { name: "   ",
 						email: "foobar@com.pl",
 						password: "bar",
 						password_confirmation: "domingo" }
-		after_count = User.count
-		assert_equal before_count, after_count
-		assert_template 'users/new'
 	end
+
+	assert_template 'users/new'
+end
+
+
+test "Should be able to sign up" do
+		get signup_path
+		assert_difference "User.count", 1 do
+			post_via_redirect users_path, user: { name: "Josh",
+						email: "foobar@com.pl",
+						password: "domingo",
+						password_confirmation: "domingo" }
+	end
+
+	assert_template 'users/show'
+end
+
+
+
+
+
 
 
 end
