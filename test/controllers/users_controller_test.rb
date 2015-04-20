9ec4ce29e6bc4d3b6d1717251a10617ffebab4ed
Velsu <riftmage50@gmail.com>
2015-04-20 21:14:11 +0200

    Finish CRUD for users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,6 +44,21 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should restrict destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when not logged in without admin priv" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
 
 
 
