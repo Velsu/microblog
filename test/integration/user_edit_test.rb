48dbcb685997c4d6ade371250a9fe7e990310fc6
Velsu <riftmage50@gmail.com>
2015-04-21 13:17:18 +0200

    Code refactor for form in new and edit templates, write additional security tests

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -22,6 +22,7 @@ end
 
 	test "successful edit with friendly forwarding" do
 		get edit_user_path(@user)
+		assert_not_nil session[:forwarding_url]
 		log_in_as(@user)
 		assert_redirected_to edit_user_path(@user)
 		name = "Foo Bar"
@@ -35,6 +36,7 @@ end
 
 		assert_not flash.empty?
 		assert_redirected_to @user
+		assert_nil session[:forwarding_url]
 		@user.reload
 		assert_equal @user.name, name
 		assert_equal @user.email, email
