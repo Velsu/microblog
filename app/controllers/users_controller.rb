5032cdd2c05d9d275c56aaab89c8f5c2a84a5c97
Velsu <riftmage50@gmail.com>
2015-04-26 23:24:42 +0200

    Configure Micropost controller, add tests, add views for microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activated?
   end
 
@@ -63,13 +64,6 @@ class UsersController < ApplicationController
 
   # method for before filter to confirm that user is logged in
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in"
-      redirect_to login_url
-    end
-  end
 
   def correct_user
     @user = User.find(params[:id])
