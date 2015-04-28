88853478a6965a5b6c668245b9244c9076452062
Velsu <riftmage50@gmail.com>
2015-04-28 22:37:09 +0200

    Complete Microposts for users, add and configure images for development and production

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     test "layout links" do
-      skip
+    skip
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
