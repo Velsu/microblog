06bd9c54fe626a32a1ead3c779134c98a6447240
Velsu <riftmage50@gmail.com>
2015-04-28 22:53:56 +0200

    Fix critical error in Static Pages Controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     test "layout links" do
-    skip
   	get root_path
   	assert_template 'static_pages/home'
   	assert_select "a[href=?]", root_path, count: 2
